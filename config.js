28cedd996aad3e086de5ac03a47af39285a217e2
Shazfa <f891d5x1838@student.devacademy.id>
2025-12-12 10:07:10 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
-// config.js
-const NODE_API_URL = "https://learnify-backend-2exd.onrender.com";
-const ML_API_URL   = "https://learnify-ml.onrender.com";
+export const NODE_API_URL = "https://learnify-backend-2exd.onrender.com";
+export const ML_API_URL   = "https://learnify-ml.onrender.com";
